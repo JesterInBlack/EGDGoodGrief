b1132af0c7a2316ee8ec5748b76a525a5de85b4f
Gabriel Violette <phoniks7@hotmail.com>
2014-03-30 14:47:13 -0400

    Changed gitignore to allow TagManager to be tracked. Added script assemblies to gitignore. Added item usage icons.

---
+++ Assets/_Scripts/ItemAnimation.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemAnimation : MonoBehaviour 
+{
+	//A class to display an item's icon over the player using it.
+
+	#region vars
+	private SpriteRenderer mySpriteRenderer;
+	private Player player;
+	private ItemName prevName;
+	#endregion
+
+	// Use this for initialization
+	void Start () 
+	{
+		mySpriteRenderer = GetComponent<SpriteRenderer>(); //amortize getcomponent cost
+		player = this.gameObject.transform.parent.GetComponent<Player>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if ( prevName != player.items[ player.itemIndex ].name ) //changed this frame
+		{
+			//change image
+			mySpriteRenderer.sprite = ItemImages.getSprite ( player.items[ player.itemIndex ].name );
+		}
+		prevName = player.items[ player.itemIndex ].name;
+
+		//show / hide.
+		if ( player.state == "item windup" || 
+		     player.state == "item charge" || 
+		     player.state == "item aim ray" || 
+		     player.state == "item aim point" )
+		{
+			mySpriteRenderer.enabled = true;  //show it.
+		}
+		else
+		{
+			mySpriteRenderer.enabled = false; //hide it.
+		}
+	}
+}
--- Assets/_Scripts/ItemImages.cs
+++ Assets/_Scripts/ItemImages.cs
@@ -11,9 +11,17 @@ public static class ItemImages
 	public static Texture2D auraHeal;
 	public static Texture2D vampireFang;
 	//public Texture2D humanShield;
+
+	public static Sprite phermoneJarSprite;
+	public static Sprite stopWatchSprite;
+	public static Sprite auraOffenseSprite;
+	public static Sprite auraDefenseSprite;
+	public static Sprite auraHealSprite;
+	public static Sprite vampireFangSprite;
+	//public static Sprite humanShieldSprite;
 	#endregion
 
-	public static void Start() //initialization proxy.
+	public static void Start() //initialization proxy. (note this is not a monobehaviour)
 	{
 		//phermoneJar = Resources.Load<Texture2D>( "_Art/GUI/Common/Items/PheromoneJar" );
 		phermoneJar = Resources.Load<Texture2D>( "PheremoneJar" );
@@ -22,10 +30,18 @@ public static class ItemImages
 		auraDefense = Resources.Load<Texture2D>( "DefensiveAura" );
 		auraHeal    = Resources.Load<Texture2D>( "HealingAura" );
 		vampireFang = Resources.Load<Texture2D>( "VampFang" );
+
+		phermoneJarSprite = Resources.Load<Sprite>( "PheremoneJar" );
+		stopWatchSprite   = Resources.Load<Sprite>( "Stopwatch" );
+		auraOffenseSprite = Resources.Load<Sprite>( "Offensive Aura" );
+		auraDefenseSprite = Resources.Load<Sprite>( "DefensiveAura" );
+		auraHealSprite    = Resources.Load<Sprite>( "HealingAura" );
+		vampireFangSprite = Resources.Load<Sprite>( "VampFang" );
 	}
 
 	public static Texture2D getImage( ItemName itemName )
 	{
+		//gets the Texture2D (GUI) version of it
 		if ( itemName == ItemName.AURA_DEFENSE )
 		{
 			return auraDefense;
@@ -52,5 +68,35 @@ public static class ItemImages
 		}
 		//default:
 		return auraDefense;
+	}
+
+	public static Sprite getSprite( ItemName itemName )
+	{
+		if ( itemName == ItemName.AURA_DEFENSE )
+		{
+			return auraDefenseSprite;
+		}
+		else if ( itemName == ItemName.AURA_OFFENSE )
+		{
+			return auraOffenseSprite;
+		}
+		else if ( itemName == ItemName.AURA_REGEN )
+		{
+			return auraHealSprite;
+		}
+		else if ( itemName == ItemName.PHEROMONE_JAR )
+		{
+			return phermoneJarSprite;
+		}
+		else if ( itemName == ItemName.STOPWATCH )
+		{
+			return stopWatchSprite;
+		}
+		else if ( itemName == ItemName.VAMPIRE_FANG )
+		{
+			return vampireFangSprite;
+		}
+		//default:
+		return auraDefenseSprite;
 	}
 }
