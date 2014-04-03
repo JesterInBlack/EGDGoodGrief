74267e8fd5f676a979240bb83d90611d8c99bfd9
Jay Lee <RamenNoodles452@gmail.com>
2014-04-03 18:55:05 -0400

    Changed Prefabs. Dislocated radius points from the main body

--- Assets/_Scripts/SpiderBoss/RadiusScript.cs
+++ Assets/_Scripts/SpiderBoss/RadiusScript.cs
@@ -1,25 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LegShadow : MonoBehaviour 
+public class RadiusScript : MonoBehaviour 
 {
-
-	public GameObject _parentLeg;
-	private LegScript _legScript;
+	public GameObject _parent;
 	private Vector3 _pos = new Vector3(0, 0, 0);
-
+	private Vector3 _offset;
+	
 	// Use this for initialization
 	void Start () 
 	{
-		_legScript = _parentLeg.GetComponent<LegScript>();
+		_offset = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		_pos = _legScript._shadowPos;
-		_pos.z = 10.0f;
+		_pos = _parent.transform.position;
+		_pos += _offset;
 		transform.position = _pos;
 	}
-
 }
