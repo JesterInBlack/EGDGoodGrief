e99c49b855d60f9b929e1584bdac47f624f49c20
Jay Lee <RamenNoodles452@gmail.com>
2014-04-13 16:53:28 -0400

    Exploding Eggsac Completed

--- Assets/_Scripts/SpiderBoss/BossManager.cs
+++ Assets/_Scripts/SpiderBoss/BossManager.cs
@@ -43,7 +43,7 @@ public class BossManager : MonoBehaviour
 		//Debug.Log(allBehaviors.Length);
 		for(int i = 0; i < allBehaviors.Length; i++)
 		{
-			if(allBehaviors[i].group == 9) //WebTether
+			if(allBehaviors[i].group == 10) //WebTether
 			{
 				testBehavior = new BehaviorData(allBehaviors[i], 1f, -1f, 1f, -1f, 25);
 				_behaviorList.Add(testBehavior);
