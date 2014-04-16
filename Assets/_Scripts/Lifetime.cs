b88b41c73dacc9d0b0c7203abaec752990d03242
Gabriel Violette <phoniks7@hotmail.com>
2014-04-16 14:23:52 -0400

    Added a script to destroy prefabs after an elapsed period of time. Fixed a bug with lava prefabs sticking around forever.

---
+++ Assets/_Scripts/Lifetime.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lifetime : MonoBehaviour 
+{
+	#region vars
+	public float lifetime = 2.0f; //lifetime in seconds
+	private float timer = 0.0f;
+	#endregion
+
+	// Use this for initialization
+	void Start () 
+	{
+		timer = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		timer += Time.deltaTime * StaticData.t_scale;
+		if ( timer >= lifetime )
+		{
+			Destroy ( this.gameObject );
+		}
+	}
+}
