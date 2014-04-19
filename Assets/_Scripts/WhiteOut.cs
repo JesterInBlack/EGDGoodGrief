1036ca1131005c026d3b2fbd1839dc8f10573a72
Gabriel Violette <phoniks7@hotmail.com>
2014-04-19 17:29:03 -0400

    Added a whiteout effect for mega flare.

---
+++ Assets/_Scripts/WhiteOut.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WhiteOut : MonoBehaviour 
+{
+	private float t;
+	private SpriteRenderer mySpriteRenderer;
+
+	// Use this for initialization
+	void Start () 
+	{
+		t = 0.0f;
+		mySpriteRenderer = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// 0.0f -> 1.0f -> 1.0f -> 0.0f
+		//  _
+		// / \
+		t += Time.deltaTime  * StaticData.t_scale;
+
+		float lerpT = Mathf.Min ( 1.0f, t );
+		if ( t > 1.0f && t < 2.0f )
+		{
+			lerpT = 1.0f;
+		}
+		else if ( t > 2.0f )
+		{
+			lerpT = Mathf.Max ( 0.0f, 3.0f - t );
+		}
+
+		if ( t > 3.0f )
+		{
+			Destroy ( this.gameObject );
+		}
+
+		mySpriteRenderer.color = new Color( 1.0f, 1.0f, 1.0f, lerpT );
+	}
+}
