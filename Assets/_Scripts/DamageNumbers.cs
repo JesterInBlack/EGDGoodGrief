c03039737c0db58ccba36e5675d7a3710aa62fb1
Gabriel Violette <phoniks7@hotmail.com>
2014-04-25 01:49:07 -0400

    Added placeholder script for damage numbers. Healing pools now Hue lerp as they get depleted. Players now steal 25% of their damage in score from players they hit. Players now deal 2.5% of their damage to other players they hit.

---
+++ Assets/_Scripts/DamageNumbers.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageNumbers : MonoBehaviour 
+{
+	//TODO: make this script get called by scoremanager
+	//TODO: make this script call players show text for defensive. For offensive, place own text at location.
+	//TODO: link to player.
+	//TODO: record player taking / dealing / blocking damage over time. (3 separate timers) (reset on new damage)
+	//TODO: improvment: 1 timer per attacker id (4 + 1, track independently)
+	//TODO: if the timer runs out, put a damage number up on the screen.
+
+	#region vars
+	private const float takeDelay = 0.1f;   //how long, in seconds, needs to pass before an attack is considered separate.
+	private const float dealDelay = 0.1f;   //how long, in seconds, needs to pass before an attack is considered separate.
+	private const float blockDelay = 0.1f;  //how long, in seconds, needs to pass before an attack is considered separate.
+	#endregion
+
+	void Awake()
+	{
+		//player = GetComponent<Player>();
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
